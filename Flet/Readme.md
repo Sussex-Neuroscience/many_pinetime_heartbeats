e92c4010e284a4f56a6a07a41a193d059a70f5ab
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-08-31 17:36:06 +0100

    continuing GUI

---
+++ Flet/Readme.md
@@ -0,0 +1,2 @@
+# Flet GUI
+
--- PyQtInterface/main_ui.py
+++ PyQtInterface/main_ui.py
@@ -7,6 +7,7 @@ And will connect the other qlabinterface scripts.
 
 """
 
+# Creating the GUI
 import sys
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
@@ -14,17 +15,68 @@ from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 #Window asking you if you want to connect to watches
 #Or import pre-collected data for example test
 class IntroWindow(QWidget):
-    def __init__(self, parent: QWidget | None = ..., flags: Qt.WindowType = ...) -> None:
-        super().__init__(parent, flags)
+    def __init__(self) -> None:
+        super().__init__()
         self.initUI()
         
     def initUI(self):
+        self.setWindowTitle("Pinetime Heartbeat Interface")
+        # Setting the size of the window (X size, Y size, X cords, Y cords)
+        self.setGeometry(0, 0, 400, 400)
+
+        # Creating the buttons
+        self.scan_button = QPushButton("Scan for devices",self)
+        self.scan_button.clicked.connect(self.on_scan_button_clicked)
+        self.connect_all_button = QPushButton("Connect all devices",self)
+        self.connect_all_button.clicked.connect(self.on_connect_all_button_clicked)
+        self.connect_one_button = QPushButton("Connect one device",self)
+        self.connect_one_button.clicked.connect(self.on_connect_one_button_clicked())
+
+        # Adding the buttons to the layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.scan_button)
+        layout.addWidget(self.connect_all_button)
+        layout.addWidget(self.connect_one_button)
+        self.setLayout(layout)
+
+    def on_scan_button_clicked(self):
+        watchScan = WatchScanWindow()
+        watchScan.show()
+
+    def on_connect_all_button_clicked(self):
+        watchConA = WatchScanWindow()
+        watchConA.show()
+
+    def on_connect_one_button_clicked(self):
+        watchConO = WatchConAllWindow()
+        watchConO.show()
+
+# Window for scanning for devices
+class WatchScanWindow(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        # After the user has pressed scan button and change window scan for devices
+        exec(open("many_devices.py").read())
+
+
+# Window for connecting to one device
+class WatchConOneWindow(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
         pass
- 
-    
-class WatchConWindow(QWidget):
-    def __init__(self, parent: QWidget | None = ..., flags: Qt.WindowType = ...) -> None:
-        super().__init__(parent, flags)
+
+
+
+# Window for connecting to all devices
+class WatchConAllWindow(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
         self.initUI()
             
     def initUI(self):
@@ -32,4 +84,7 @@ class WatchConWindow(QWidget):
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    start_window = IntroWindow()
+    start_window.show()
+    sys.exit(app.exec())
     
